dc9d0cf2fdb5b39e71122bf5fb7684fa895053fd
Andrew Van De Poel <andrewvdp@gmail.com>
2021-09-16 13:30:19 -0700

    testing config

--- bin/build.bash
+++ bin/build.bash
@@ -8,3 +8,16 @@ version="test"
 echo "building image: $repo:$version"
 
 docker build -t $repo:$version .
+
+echo
+echo "Docker image $repo:$version built"
+echo "Do you want to push to docker hub?"
+select answer in "yes" "no"; do
+  case "${answer}" in
+    yes ) echo "pushing"; break;;
+    no ) echo "all done"; exit -1;;
+  esac
+done
+echo
+
+docker push $repo:$version
